7624e4c88e7076751a7a44659614a7b643abe87b
Bryce <bsjohnson311@gmail.com>
2025-11-11 11:13:36 -0800

    Update dumb-install.sh

--- dumb-install.sh
+++ dumb-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p ~/.config/tmux
-cp .tmux.conf ~/.config/tmux/.
-cp .tmux.conf.local ~/.config/tmux/.
+cp -f .tmux.conf ~/.config/tmux/tmux.conf
+cp -f .tmux.conf.local ~/.config/tmux/tmux.conf.local
